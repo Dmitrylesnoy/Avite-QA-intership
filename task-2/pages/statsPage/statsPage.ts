a4d2636020e308f7b24446058a5ec4ae456a079b
Akim Lineiskiy <vasymedov@gmail.com>
2026-04-05 21:21:29 +0300

    stats page

--- task-2/pages/statsPage/statsPage.ts
+++ task-2/pages/statsPage/statsPage.ts
@@ -5,13 +5,36 @@ export class StatsPage extends BasePage {
     protected pageName = "Cтраница статистики";
 
     readonly rootLocator: Locator;
+    readonly refreshTimerbtn: Locator;
+    readonly toggleTimerBtn: Locator;
+    readonly timerValue: Locator;
 
     constructor(page: Page) {
         super(page);
         this.rootLocator = this.page.locator("#root");
+        this.refreshTimerbtn = this.page.locator("\\button[contains(@class, 'refresh')]");
+        this.toggleTimerBtn = this.page.locator("\\button[contains(@class, 'toggle')]")
+        this.timerValue = this.page.locator("\\span[contains(@class, 'timerLabel')]");
     }
 
     protected root(): Locator {
         return this.page.locator("#root");
+    }
+
+    async clickRefreshTimerBtn() {
+        await this.refreshTimerbtn.click();
+    }
+
+    async clickToggleTimerBtn() {
+        await this.toggleTimerBtn.click();
+    }
+
+    async getToggleBtnState() {
+        const classAttr = await this.toggleTimerBtn.getAttribute('class');
+        return classAttr ? classAttr.includes('active') : false;
+    }
+
+    async getTimerValue() {
+        return await this.timerValue.textContent();
     }
 }
